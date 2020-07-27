79635639e425620a8f5b505a8cf9a429245cee85
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-07-27 15:43:48 -0600

    Created DAL and added Readall and ReadId

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('dotenv').config
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,8 +20,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//Port
+app.listen(process.env.PORT)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ data/DAL.js
@@ -0,0 +1,47 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const { ObjectId } = require('mongodb');
+const { options } = require('../app');
+
+const url = process.env.DB_URL;
+const db_name = process.env.DB_NAME;
+const col_name = process.env.COL_NAME
+const options = {
+    useUnifiedTopology: true
+}
+
+
+const readAll= () => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, options, (err, client) =>{
+            assert.equal(err, null);
+
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+            collection.find({}).toArray((err, docs) =>{
+                assert.equal(err, null);
+                resolve(docs);
+                client.close()
+            })
+        })
+    })
+    return iou
+}
+
+const readMovieById= (id) => {
+    const iou = new PromiseRejectionEvent((resolve, reject) => {
+        MongoClient.connect(utl, options, (err, client) => {
+            assert.equal(err, null);
+
+            const db = client.db(db_name);
+            const collection = db.collection(col_name);
+            collection.find({_id: new ObjectId(id)}).toArray((err, docs) => {
+                assert.equal(err, null);
+                resolve(docs[0]);
+                client.close();
+            })
+        })
+    })
+    return iou
+}
+
